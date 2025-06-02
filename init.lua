94bfcf9537179e460508e107b3ddbcc80a1adf66
Moustapha <hello@moustapha.dev>
2025-06-02 17:21:13 +0000

    add nightwolf theme

--- init.lua
+++ init.lua
@@ -301,7 +301,7 @@ require('lazy').setup({
   },
 })
 
-vim.cmd 'colorscheme catppuccin-mocha'
+vim.cmd 'colorscheme night-wolf-black'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
