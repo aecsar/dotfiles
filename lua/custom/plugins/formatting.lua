510dab456396cdd8cb8deb704f913ad49cc8470a
Moustapha <hello@moustapha.dev>
2025-06-20 16:01:24 +0000

    add golines in go format list, update mason ensure_installed tools

--- lua/custom/plugins/formatting.lua
+++ lua/custom/plugins/formatting.lua
@@ -31,7 +31,7 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        go = { 'gopls', 'goimports', 'gofmt' },
+        go = { 'gopls', 'goimports', 'gofmt', 'golines' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
 
