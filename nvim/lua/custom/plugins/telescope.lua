9fe30e6d636d7224034164f06af3ece336c8c005
Moustapha <hello@moustapha.dev>
2026-01-14 01:45:20 +0000

    update nvim telescope to enable fzf search on hidden files while ignoring .git folder

--- nvim/lua/custom/plugins/telescope.lua
+++ nvim/lua/custom/plugins/telescope.lua
@@ -47,15 +47,22 @@ return { -- Fuzzy Finder (files, lsp, etc)
       -- You can put your default mappings / updates / etc. in here
       --  All the info you're looking for is in `:help telescope.setup()`
       --
-      -- defaults = {
+      defaults = {
+        file_ignore_patterns = { "^.git/" }, -- ignore .git folder
       --   mappings = {
       --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
       --   },
-      -- },
+      },
       -- pickers = {}
       extensions = {
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),
+        },
+        fzf = {
+          fuzzy = true, -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true, -- override the file sorter
+          case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
         },
       },
     }
@@ -68,7 +75,11 @@ return { -- Fuzzy Finder (files, lsp, etc)
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>sf', function()
+      builtin.find_files {
+        hidden = true, -- show hidden
+      }
+    end, { desc = '[S]earch [F]iles (git + dotfiles)' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
