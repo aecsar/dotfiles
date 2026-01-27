36ddfc1b69fb5b2092fa9e3869cd6f250472f82f
Moustapha <hello@moustapha.dev>
2026-01-27 14:51:33 +0000

    update bufferline sort, and always show bufferline

--- nvim/lua/custom/plugins/bufferline.lua
+++ nvim/lua/custom/plugins/bufferline.lua
@@ -22,7 +22,8 @@ return {
       -- stylua: ignore
       right_mouse_command = function(n) Snacks.bufdelete(n) end,
       diagnostics = 'nvim_lsp',
-      always_show_bufferline = false,
+      always_show_bufferline = true,
+      sort_by = 'insert_at_end',
       -- diagnostics_indicator = function(_, _, diag)
       --   local icons = LazyVim.config.icons.diagnostics
       --   local ret = (diag.error and icons.Error .. diag.error .. ' ' or '') .. (diag.warning and icons.Warn .. diag.warning or '')
