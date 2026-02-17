f86f76f969cb3d71429534484790a67d615480a6
Moustapha <hello@moustapha.dev>
2026-02-17 15:43:20 +0000

    format svelte with prettier

--- nvim/lua/custom/plugins/formatting.lua
+++ nvim/lua/custom/plugins/formatting.lua
@@ -40,6 +40,7 @@ return {
         typescript = { 'prettierd', 'prettier', stop_after_first = true },
         javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        svelte = { 'prettierd', 'prettier', stop_after_first = true },
         css = { 'prettierd', 'prettier', stop_after_first = true },
         rust = { 'rustfmt' },
         -- json = { 'prettierd', 'prettier', stop_after_first = true },
