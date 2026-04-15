3d251e6224936c9ccddc3d8f11734e17dd46f7e0
Moustapha <hello@moustapha.dev>
2026-04-15 18:28:03 +0000

    disable snacks lsp keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -305,8 +305,8 @@ require('lazy').setup({
   },
 })
 
--- vim.cmd 'colorscheme tokyonight-night'
-vim.cmd 'colorscheme catppuccin-mocha'
+vim.cmd 'colorscheme tokyonight-night'
+-- vim.cmd 'colorscheme catppuccin-mocha'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
